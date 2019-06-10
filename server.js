f3d14329cf5a6ca2b81d1901d832b77e04be08a0
Daniel <daniel.ryuichi.wyatt@gmail.com>
2019-06-09 20:48:15 -0500

    added form response recording functionality

--- server.js
+++ server.js
@@ -5,7 +5,13 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
+var friends = [
+    {
+        name: "Daniel",
+        photo: "Placeholder",
+        choices: [1, 2, 4, 5, 1, 2, 3, 3, 2, 1]
+    }
+]
 app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "app/public/home.html"));
 });
@@ -14,6 +20,23 @@ app.get("/survey", function (req, res) {
     res.sendFile(path.join(__dirname, "app/public/survey.html"));
 });
 
+app.get("/api/friends", function (req, res) {
+    return res.json(friends);
+});
+
+app.post("/api/friends", function (req, res) {
+
+    var newUser = req.body;
+
+    console.log(newUser);
+
+    friends.push(newUser);
+
+    res.json(newUser);
+});
+
 app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
 });
+
+
