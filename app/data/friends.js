f8cf9f59b4a03ad8ec7edaf5f0fc92e214a1090f
Daniel <daniel.ryuichi.wyatt@gmail.com>
2019-06-12 20:15:50 -0500

    Added Modal for search result

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,23 +1,33 @@
 
 
 
-    var friends = [
-        {
-            name: "Daniel",
-            photo: "Placeholder",
-            choices: [1, 2, 4, 5, 1, 2, 3, 3, 2, 1]
-        },
-        {
-            name: "Sam",
-            photo: "Placeholder",
-            choices: [3, 2, 5, 4, 3, 1, 2, 1, 4, 5]
-        },
-        {
-            name: "Charlotte",
-            photo: "Placeholder",
-            choices: [4, 3, 2, 5, 1, 2, 4, 3, 2, 5]
-        }
-    ]
-   
+var friends = [
+    {
+        name: "Frank",
+        photo: "http://static-media.fxx.com/img/FX_Networks_-_FXX/732/391/SUN_S13_1302_SelectedScene4_Wed10pm_CLEAN_FXWEB_R.jpg",
+        choices: [1, 2, 4, 5, 1, 2, 3, 3, 2, 1]
+    },
+    {
+        name: "Charlie",
+        photo: "https://static0.srcdn.com/wordpress/wp-content/uploads/2018/03/Charlie-Day-from-Its-Always-Sunny-in-Philadelphia.jpg",
+        choices: [3, 2, 5, 4, 3, 1, 2, 1, 4, 5]
+    },
+    {
+        name: "Mac",
+        photo: "https://i.pinimg.com/originals/1e/61/57/1e61574ab399d8752199806db1b5fc5f.jpg",
+        choices: [4, 3, 2, 5, 1, 2, 4, 3, 2, 5]
+    },
+    {
+        name: "Dennis",
+        photo: "https://a1cf74336522e87f135f-2f21ace9a6cf0052456644b80fa06d4f.ssl.cf2.rackcdn.com/images/characters/p-its-always-sunny-in-philadelphia-glenn-howerton.jpg",
+        choices: [3, 5, 1, 2, 1, 2, 3, 4, 4, 1]
+    },
+    {
+        name: "Dee",
+        photo: "https://a1cf74336522e87f135f-2f21ace9a6cf0052456644b80fa06d4f.ssl.cf2.rackcdn.com/images/characters/p-its-always-sunny-in-philadelphia-kaitlin-olson.jpg",
+        choices: [2, 1, 5, 5, 5, 5, 5, 1, 1, 3]
+    }
+]
 
-module.exports = {friends};
+
+module.exports = { friends };
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,6 +1,5 @@
 
 const allDataOfFriends = require("../data/friends.js");
-console.log(allDataOfFriends);
 
 module.exports = function(app){
  
@@ -15,39 +14,22 @@ module.exports = function(app){
         var userResult = newUserChoices.map(function (x) {
             return parseInt(x, 10);
         });
-    
-        console.log(userResult);
+
         var diffNum = 0;
         var diffArray = [];
         var totalDiffArray = [];
-        var finalDiffArray =[];
-    
-    
-        console.log(newUser);
-        var newUserScore = userResult.reduce((a, b) => a + b, 0);
-        console.log("newUserScore", newUserScore);
+
         for (var i = 0; i < allDataOfFriends.friends.length; i++) {
-    
-            console.log("Here is the Loop");
-            console.log(newUser.name);
-            console.log(newUser.photo);
             for (var j = 0; j < newUser.choices.length; j++) {
-                // console.log(newUser.choices[j]);
                 diffNum = Math.abs(newUser.choices[j] - allDataOfFriends.friends[i].choices[j]);
                 diffArray.push(diffNum);
-                console.log(diffArray);
                 var totalDiff = diffArray.reduce((a, b) => a + b, 0)
-                console.log(totalDiff);
             }
             totalDiffArray.push(totalDiff);
             diffArray.length = 0;
-            console.log("Total Diff Array: ", totalDiffArray);
         }
         var finalDiff = Math.min(...totalDiffArray);
-        console.log(finalDiff);
         var indexOfMin = totalDiffArray.indexOf(finalDiff);
-        console.log(allDataOfFriends.friends[indexOfMin]);
-        // friends.push(newUser);
         res.json(allDataOfFriends.friends[indexOfMin]);
     
     });
